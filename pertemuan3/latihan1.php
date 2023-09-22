93b91b020b55ee9ac35ffdba7517e679b2f58b58
uphi13 <144438325+uphi13@users.noreply.github.com>
2023-09-23 03:49:26 +0700

    Add files via upload

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        table, th, td {
+            border: 2px solid;
+            border-collapse: collapse;
+            border-color: #96D4D4;
+        }
+        /* th {
+            align: "left";
+        } */
+    </style>
+    <title>Latihan</title>
+</head>
+<body>
+    <center><h1>Biodata</h1>
+    <table>
+        <tr>
+            <th align="left">Nama</th>
+            <td>:</td>
+            <td><?php echo "Luthfi Awaludin"; ?></td>
+        </tr>
+        <tr>
+            <th align="left">Tempat Lahir</th>
+            <td>:</td>
+            <td><?php echo "Tasikmalaya"; ?></td>
+        </tr>
+        <tr>
+            <th align="left">Tanggal Lahir</th>
+            <td>:</td>
+            <td><?php echo "13 November 2003"; ?></td>
+        </tr>
+        <tr>
+            <th align="left">Hobi</th>
+            <td>:</td>
+            <td><?php echo "Bermain Musik Gitar"; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
